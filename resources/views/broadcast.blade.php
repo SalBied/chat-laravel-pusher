48ab43a116353776c4887016b28f702433868924
Salah Obied <salahuobed14@gmail.com>
2024-09-21 02:02:07 +0300

    simple chat app

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+    <p>{{$message}}</p>
+    <img src="https://picsum.photos/id/1/200/300" alt="Profile picture">
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+    <img src="https://picsum.photos/id/1/200/300" alt="Avatar">
+    <p>{{$message}}</p>
+</div>
